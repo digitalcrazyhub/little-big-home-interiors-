340bc9a7f62a58fd9c2614c75251137eda178dd1
Devendran <digitalcrazyhub2020@gmail.com>
2026-08-12 19:10:23 +0530

    undo all project file

--- src/main/java/com/example/littlebighome/gallery/contact/ContactLeadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.littlebighome.gallery.contact;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactLeadRepository
-        extends JpaRepository<ContactLead, Long> {
-}
